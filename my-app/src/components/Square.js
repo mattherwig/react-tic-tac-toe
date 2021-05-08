5f6bc2d1674d97318e09fae8d43b85357478a184
BuildTools <herwig.matt@gmail.com>
2021-05-08 14:46:55 -0400

    Clean up

--- my-app/src/components/Square.js
+++ my-app/src/components/Square.js
@@ -2,7 +2,6 @@ import React from 'react';
 import '../index.css';
 
 export default function Square(props) {
-  console.log('props.isWinnerTile', props.isWinnerTile);
   return (
     <button 
       className={"square " + (props.isWinnerTile ? "winner" : "")}
